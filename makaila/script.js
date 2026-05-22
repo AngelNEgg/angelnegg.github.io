3fac76f7d0aa132a4699ef0870638489e9af6702
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2026-05-21 21:47:31 -0400

    Add files via upload

---
+++ makaila/script.js
@@ -0,0 +1,44 @@
+var groupTitle;
+var groupDesc;
+var groupCont;
+var groupTag;
+
+groupPar = "Learning to Commmunicate";
+groupTitle = "Makailas_Diary";
+
+groupDesc =
+  "Welcome to my little internet diary <3 leave a comment & don’t steal my song off my profile :)";
+
+groupCont1 =
+  "Many people believe that school is only meant for learning academic subjects such as math, science, English, or history. While these subjects are important, school serves a much bigger purpose than just the basic academics. School helps students develop communication skills, build relationships with their peers, and prepare for college and future careers. Through daily interactions, teamwork, and shared experiences, students learn important life skills that help them grow both personally and socially—making school not just a place for education, but also a place where students learn how to succeed in the real world.";
+
+groupCont2 =
+  "In classrooms, students are communicating everyday. Whether they are talking, listening, asking or answering questions, and sharing ideas with one another. A study on learning communities in student classrooms of middle school students say that, “communicative ability significantly influences learning outcomes.” This is basically saying that students who know how to express themselves clearly mostly do better in school. Learning to communicate effectively also helps students to feel more confident and less anxious. By using and practicing these skills daily, students learn how to speak up, work through challenging situations, and understand others better.";
+
+groupCont3 =
+  "To add to communication, school is a place where students learn how to build relationships with their peers. Being around other students everyday gives students the chance to form new friendships, connections, and learn how to interact socially. Research on peer relationships shows that, “peer relationships are essential for social adjustment.” Learning how to get along with others is necessary for personal growth. Students learn teamwork, respect, and how to handle conflicts. These experiences help shape how they behave and interact with others outside of school also. ";
+
+groupCont4 =
+  "School also prepares students for the future, especially for college and careers. Many classes involve group work, projects, and debates that require students to work together. A researcher Robert E Slavin argues that, “cooperative learning promotes higher achievement and interpersonal skills.” Working with others not only improves grades but also teaches important life skills. In college and in jobs, people are expected to collaborate, communicate clearly, and solve problems as a team. School gives students a chance to practice these skills earlier so that they are ready for what comes after.";
+
+groupCont5 =
+  "Incorporating my own personal experience, interactions in school also affect students' motivation and success. I remember when I first got into high school I was so nervous. I was scared to even approach anyone, but after making a friend in my class it encouraged me to not only get the confidence to make new friends, but to work harder in the classroom to achieve higher grades so that I could help support my friends as well. Research by Kathryn R. Wentzel explains that “peer relationships play a crucial role in students' academic motivation.” When students feel supported by their peers, they are more likely to try harder and stay engaged in their work—showing that school is not just about individual learning, but also being part of a community that helps each other grow.";
+
+groupCont6 =
+  "To close this argument, school is not just a place for academic subjects. School is for helping students develop their communication skills,build relationships, and prepare for their futures. These skills are important for success in college, future careers and everyday life. The research clearly shows that interacting with others and learning how to communicate are very important benefits of going to school. Overall, school plays a big role in shaping students into well-rounded individuals who are getting  ready for the real world.";
+
+groupTag = 'Content by "Makaila Sampson"';
+
+$(document).ready(function () {
+  groupTitle = $("#title").text(groupTitle);
+  groupPar = $("#par").text(groupPar);
+  groupDesc = $("#desc").text(groupDesc);
+  groupCont1 = $("#cont1").text(groupCont1);
+  groupCont2 = $("#cont2").text(groupCont2);
+  groupCont3 = $("#cont3").text(groupCont3);
+  groupCont4 = $("#cont4").text(groupCont4);
+  groupCont5 = $("#cont5").text(groupCont5);
+  groupCont6 = $("#cont6").text(groupCont6);
+  groupTag = $("#tag").text(groupTag);
+  $("body").fadeIn(1000);
+});
