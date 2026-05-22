75192076bc8ae5485f740c449d4191ce6bc7a874
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2026-05-21 23:35:11 -0400

    Update and rename script.js to scripKay.js

---
+++ kayla/scripKay.js
@@ -0,0 +1,33 @@
+var groupTitle;
+var groupDesc;
+var groupCont;
+var groupTag;
+
+groupTitle = 'Kayla_TanishaBloom';
+groupPar = 'Developing Character'
+groupDesc = 'Where values become habits';
+
+groupCont1 = 'People always say that high school is one of the most crucial experiences in your life.  School is often seen as a place for academic learning, but is that really the main purpose? From freshman to senior year, students grow not only intellectually but also socially and emotionally. Character development is the process of growing into a more mature, responsible, and self-aware individual. When students take small steps, they gradually build important qualities, such as collaboration, perseverance, accountability, and maturity.';
+
+groupCont2 = "Through experiences, we form and lose relationships, we learn through communication skills and taking on responsibility. From Kayla's experience, she describes her sophomore year as a major turning point. She formed close friendships and considered some of her peers like family but at the end of the year, those friendships fell apart. This experience was very painful but it taught her about trust, resilience, and how complicated relationships can be. Similarly, Tanisha shares how she started high school as a shy and quiet girl, who barely talked to anyone. Over the course of these four years, she pushed herself to get out of her shell, especially after setting goals in her English class, and now she has many great friends, feels more comfortable around people, and tries her best to participate in school. School pushes us out of our comfort zones, encourages us to take new steps, and helps us grow into more confident and capable individuals.";
+
+groupCont3 = 'The article More Than Grades: How Schools Build Character plays an important role in character development. As mentioned by one of the authors, “Presentation of Learning (POL) makes character measurable. You have to provide evidence with specific examples. Students demonstrate character growth with concrete instances: commitments kept, challenges navigated, quality pursued despite obstacles.” Schools help build character by requiring students to prove their behavior with real examples, not just to talk about it. By demonstrating collaboration, accountability, and perseverance, students actively practice and reflect on these traits. This process helps turn good habits into lasting parts of the character. In the article Character Education, Prevention, and Positive Youth Development, as mentioned by Battistich, “By providing youth with a supportive school environment, especially during their childhood years, their psychological needs to feel that they belong to a community where they are competent and influential will be satisfied, and they will therefore want to remain part of the school community and be inclined to adopt its positive social norms and values.” When students feel supported and feel like they belong, they are more likely to develop positive traits such as responsibility, respect, and confidence. A supportive environment encourages students to make good choices and follow positive values. Over time, these behaviors become part of their character.';
+
+groupCont4 = 'Some people may argue that school is not just for character development but also mainly for academic purposes. As Mike Rose mentions in the article, Character Education: A Cautionary Note, “For character-building interventions to have an effect on academic achievement, students need a curriculum that is academically substantial.” This can distract students from their primary role of learning academically. Schools have limited time and resources, and focusing too much on personal growth may take away their time from learning the main subjects that will help them succeed in the future. Learning academically is not the sole focus of school, and character development is as essential as intellectual learning. While the opposing side may be correct, character development is still very essential to everyday life because academic learning teaches what to know, but character development teaches how to navigate life, handle challenges, and succeed in different situations. Knowledge alone isn’t enough without a strong character. It helps students build more values like responsibilities, respect, and teamwork, which are necessary in life.';
+
+groupCont5 = 'Character development is a key part of the school culture. However, some students may not experience the same level of growth and may even face setbacks; school still provides opportunities for further development. Through relationships, real-world experiences, and personal growth, students build skills and values needed for the future. School helps shape not just what we know but also who we become as a person. School is primarily a space where character is shaped and strengthened over time, and a way to shape our mindsets.';
+
+groupTag = 'Content by Kayla and Tanisha';
+
+$(document).ready(function () {
+  groupTitle = $("#title").text(groupTitle);
+  groupPar = $("#par").text(groupPar);
+  groupDesc = $("#desc").text(groupDesc);
+  groupCont1 = $("#cont1").text(groupCont1);
+  groupCont2 = $("#cont2").text(groupCont2);
+  groupCont3 = $("#cont3").text(groupCont3);
+  groupCont4 = $("#cont4").text(groupCont4);
+  groupCont5 = $("#cont5").text(groupCont5);
+  groupTag = $("#tag").text(groupTag);
+  $('body').fadeIn(1000);
+});
