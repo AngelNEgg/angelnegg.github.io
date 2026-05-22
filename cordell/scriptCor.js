87a00ef6b1b1cde49c6ebc820cc975a4869bf9eb
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2026-05-22 08:53:43 -0400

    Update and rename script.js to scriptCor.js

---
+++ cordell/scriptCor.js
@@ -0,0 +1,51 @@
+var groupTitle;
+var groupDesc;
+var groupCont;
+var groupTag;
+
+groupTitle = 'MetaphorMuncher';
+groupPar = "Uncomfortable Situations";
+
+groupDesc = '"School helps build social skills by placing students in a shared environment with other students where they must learn how to navigate throughout their years in school with cooperation, conflict and communication."';
+
+groupCont1 = "The discomfort of Students in the school! School is where students are meant to learn, grow and prepare themselves for the future. Well, for most teens nowadays, school is where stress, anxiety and burnout .These feelings of discomfort negatively affect a student's mental and emotional well being. Instead they are often caused by heavy workloads, lack of sleep and consistent academic pressure are not what seek to assist the well being of students, it is all that unhealthy school workload, without sleep and academic pressure in a modern school system. If the goal is actually to set students up for success, schools must transition toward prioritizing students'mental health and physical health.";
+
+groupCont2 = 'Extreme academic pressure and work load is the leading cause of discomfort of students within school. It expects many students to juggle between homework tests and extracurricular activities, and college preparations all at the same time. According to” New York Times Learning Network discussions, students frequently report feeling stressed and burned out by the constant pressure to perform well. When school becomes focused only on grades and test scores, learning stops feeling meaningful and starts feeling like a competition. This pressure can make students anxious and less motivated, which goes against the purpose of education.”';
+
+groupCont3 = "From the moment I entered high school as a freshman, I felt that pressure. I constantly questioned why passing a few exams was treated as the hard requirement for graduation, even when I was already putting in the work, passing my classes, and earning credits. No matter how much effort students put in failing a single exam can make them feel as though they are not good enough.This creates the impression that students' value is determined only by academic pressure. I constantly felt overwhelmed by the pressure to meet academic expectations. The fear of failure, the fear of getting left out and disappointment of others weighed heavily on me. It made me feel like my entire future depended on a few test scores. Furthermore those exams failed to reflect the effort, the growth, and personal challenges I overcame throughout high school. Despite that, I kept pushing myself forward, I studied hard, I struggled and eventually I passed. But that doesn’t change the fact that the system itself is wrong. No student deserves or should have to feel like their worth and potential is determined by an exam. Education should be about learning growth, and opportunity not fear, pressure, and survival.";
+
+groupCont4 = "I experienced growth with myself, throughout high school from freshman to junior year. I've really been slacking in some classes either passing or barely passing. I even had summer school freshman year. I thought high school was going to be challenging until I managed to work through those challenges high school gave and managed to keep my grades up. I was managing to turn work in before the set deadlines. I used to like being in small classes because it wasn't as many kids that I thought would judge me but once I actually tried the bigger classes in senior year, it wasn't as bad, i’ve still been scared to speak in bigger classes but once I got used to being in bigger groups and talking to other new people and doing bigger collab projects in bigger classes (Ex. The Hamlet project)  it felt like I sort of came out of my comfort zone being with a bigger crowd. Freshman year i’ve been quiet and kept to myself and didn't really ask for help from any of my teachers, I had a horrible performance with grades and was talking a laughing with friends and disrupting the class and getting in trouble with my mom because of it which pushed me to do better in school and get higher grades.";
+
+groupCont5 = 'Another significant issue is the lack of sleep caused by early school schedules. Teenagers need about eight to ten hours of sleep each night to function properly, yet many are forced to wake up very early for school. This often leads to exhaustion, difficulty concentrating, and lower academic performance. When students are consistently tired, it becomes much harder for them to learn effectively. If schools are meant to help students succeed, they should not place them in routines that harm their physical and mental health.';
+
+groupCont6 = 'School can create emotional discomfort because students often feel they are not being heard. Many students believe their opinion about school is being ignored, even though they are the ones who experience school every day. When students feel they have no voice, school can feel unfair and frustrating. Giving students opportunities to share their ideas and experiences could make school feel more supportive and welcoming.';
+
+groupCont7 = 'Some people urge that stress and academic pressure prepare students for real world responsibilities.  They believe that school should be demanding in order to teach responsibility and discipline. While some level of challenges are important, too much pressure can have negative effects. When students feel overwhelmed, they may lose motivation and struggle with their mental health. A healthy balance between challenge and support is necessary.';
+
+groupCont8 = 'In conclusion school should be a place where students feel supported, motivated and ready to learn. However the current system often creates discomfort through stress, lack of sleep and limited student voice. If education is meant to prepare students for the future, it must focus not only on academic success but also on overall well being. By listening to students and creating healthier learning environments schools can better fulfill their purpose.';
+
+groupCont9 = 'Group blog writing here';
+
+groupCont10 = 'Group blog writing here';
+
+groupTag = 'Content by "Cordell and Muhammad"';
+
+$(document).ready(function () {
+  groupPar = $("#par").text(groupPar);
+  groupTitle = $("#title").text(groupTitle);
+  groupDesc = $("#desc").text(groupDesc);
+  
+  groupCont1 = $("#cont1").text(groupCont1);
+  groupCont2 = $("#cont2").text(groupCont2); 
+  groupCont3 = $("#cont3").text(groupCont3);
+  groupCont4 = $("#cont4").text(groupCont4);
+  groupCont5 = $("#cont5").text(groupCont5);
+  groupCont6 = $("#cont6").text(groupCont6);
+  groupCont7 = $("#cont7").text(groupCont7);
+  groupCont8 = $("#cont8").text(groupCont8);
+  groupCont9 = $("#cont9").text(groupCont9);
+  groupCont10 = $("#cont10").text(groupCont10);
+  
+  groupTag = $("#tag").text(groupTag);
+  $('body').fadeIn(1000);
+});
